10b07393cc42b651453d43760e88ce2c70387264
Runar Ovesen Hjerpbakk <hjerpbakk@gmail.com>
2016-11-17 11:05:18 +0100

    Added DIPS Color

--- src/EmployeeApp/App/Acquaint.Native/Acquaint.Native.iOS/Extensions/UINavigationControllerExtensions.cs
+++ src/EmployeeApp/App/Acquaint.Native/Acquaint.Native.iOS/Extensions/UINavigationControllerExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using UIKit;
+using Hjerpbakk.Common.iOS.UIKit;
 
 namespace Acquaint.Native.iOS
 {
@@ -11,10 +12,13 @@ namespace Acquaint.Native.iOS
 
 			// set some stlye properties foe the navigation bar
 			UINavigationBar.Appearance.TintColor = UIColor.White;
-			UINavigationBar.Appearance.BarTintColor = UIColor.FromRGB(84, 119, 153);
+			UINavigationBar.Appearance.BarTintColor = UIColor.Clear.FromHex(0xD32D28);
 			UINavigationBar.Appearance.TitleTextAttributes = new UIStringAttributes { ForegroundColor = UIColor.White };
 			UIBarButtonItem.Appearance.SetTitleTextAttributes(new UITextAttributes { TextColor = UIColor.White }, UIControlState.Normal);
 		}
+
+
 	}
+
 }
 
