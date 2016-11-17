bdbfbf326cffd84e8cc8f81a6122729a0e4b7fe1
Runar Ovesen Hjerpbakk <hjerpbakk@gmail.com>
2016-11-17 10:52:10 +0100

    Added App

---
+++ src/EmployeeServer/Model/Employee.cs
@@ -0,0 +1,24 @@
+﻿namespace KitchenResponsible.Model
+{
+    public class Employee
+    {
+        public Employee(ushort weekReponsible, string firstName, string lastName, string nick = null)
+        {
+            WeekResponsible = weekReponsible;
+            FirstName = firstName;
+            LastName = lastName;
+            Nick = nick ?? firstName; 
+
+            JobTitle = "UX Utvikler";
+            Company = "Health Angels";
+        }
+
+        public ushort WeekResponsible { get; set; }
+        public string Nick { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public string Company { get; }
+        public string JobTitle { get; }
+    }
+}
