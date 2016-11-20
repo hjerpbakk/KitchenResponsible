abb2b08501e1540f16e70043b91c190348379234
Runar Ovesen Hjerpbakk <hjerpbakk@gmail.com>
2016-11-20 21:39:43 +0100

    Oh so pretty

--- src/EmployeeServer/Data/TeamMembersRepo.cs
+++ src/EmployeeServer/Data/TeamMembersRepo.cs
@@ -4,16 +4,13 @@ using Microsoft.Data.Sqlite;
 namespace KitchenResponsible.Data {
     public class TeamMembersRepo {
         public IList<string> Test() {
-            using (var connection = new SqliteConnection("Filename=" + "Trondheim.db"))
-            {
+            using (var connection = new SqliteConnection("Filename=" + "Trondheim.db")) {
                 connection.Open();
                 var command = connection.CreateCommand();
                 command.CommandText = "SELECT Nick FROM TeamMembers";
                 var nicks = new List<string>();
-                using (var reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
+                using (var reader = command.ExecuteReader()) {
+                    while (reader.Read()) {
                         nicks.Add(reader.GetString(0));
                     }
                 }
