40d23bedee320f9078ff1b7a09c14ceb7d2d7d22
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2016-12-05 21:17:04 +0100

    Removed bootstrap

--- src/EmployeeServer/wwwroot/_references.js
+++ src/EmployeeServer/wwwroot/_references.js
@@ -1,6 +1,5 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="js/site.js" />
-/// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
-/// <reference path="lib/jquery/dist/jquery.js" />
-/// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
-/// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
+﻿<autosync enabled="true" />
+<reference path="js/site.js" />
+<reference path="lib/jquery/dist/jquery.js" />
+<reference path="lib/jquery-validation/dist/jquery.validate.js" />
+<reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
