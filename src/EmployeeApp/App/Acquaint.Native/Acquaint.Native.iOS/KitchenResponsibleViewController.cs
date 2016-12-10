8fa417bba69d6072a6277361b39e4fdd0e383b30
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2016-12-10 09:35:20 +0100

    Disallowed selection

--- src/EmployeeApp/App/Acquaint.Native/Acquaint.Native.iOS/KitchenResponsibleViewController.cs
+++ src/EmployeeApp/App/Acquaint.Native/Acquaint.Native.iOS/KitchenResponsibleViewController.cs
@@ -20,6 +20,8 @@ namespace Acquaint.Native.iOS
 		public KitchenResponsibleViewController(IntPtr handle) : base(handle)
 		{
 			TableView.RegisterClassForCellReuse(typeof(WeekCell), WeekCellId);
+			TableView.AllowsSelection = false;
+
 			tableViewSource = new KitchenResponsibleTableViewSource();
 		}
 
