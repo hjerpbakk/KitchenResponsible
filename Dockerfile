599f4143a7b25210f43d0c264c1b57714105685d
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2017-09-26 18:36:36 +0200

    Added Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM microsoft/aspnetcore:2.0
+WORKDIR /app
+COPY ./publish .
+ENTRYPOINT ["dotnet", "KitchenResponsibleService.dll"]
--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+rm -r ./publish
 dotnet restore
 dotnet build
-dotnet test
-dotnet publish -o ../../publish -c Release
+dotnet test src/KitchenResponsibleServiceTests/KitchenResponsibleServiceTests.csproj --no-build --no-restore
+# TODO: Fail i tests are red...
+dotnet publish src/KitchenResponsibleService/KitchenResponsibleService.csproj -o ../../publish -c Release
+docker build -t kitchen-responsible .
