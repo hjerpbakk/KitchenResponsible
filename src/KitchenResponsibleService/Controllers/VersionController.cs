484dc296b459d41d964660485cafdf948f9cebac
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2017-10-23 11:22:08 +0200

    Added version API

---
+++ src/KitchenResponsibleService/Controllers/VersionController.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KitchenResponsibleService.Controllers
+{
+    [Route("api/[controller]")]
+    public class VersionController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+    }
+}
